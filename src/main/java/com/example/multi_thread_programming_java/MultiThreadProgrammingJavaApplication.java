fa3ecc7f966e03ad7653f57261a1d5dc459a4db8
Yuting Wang <yw4554@nyu.edu>
2024-03-25 18:38:57 -0500

    thread multi

--- src/main/java/com/example/multi_thread_programming_java/MultiThreadProgrammingJavaApplication.java
+++ src/main/java/com/example/multi_thread_programming_java/MultiThreadProgrammingJavaApplication.java
@@ -7,7 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MultiThreadProgrammingJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultiThreadProgrammingJavaApplication.class, args);
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+
+        t1.setName("线程1");
+        t2.setName("线程2");
+
+
+        t1.start();
+        t2.start();
+
+
+
+        //SpringApplication.run(MultiThreadProgrammingJavaApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/example/multi_thread_programming_java/MyThread.java
@@ -0,0 +1,12 @@
+package com.example.multi_thread_programming_java;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(getName() + "Helloworld");
+            //线程是交替的
+
+        }
+    }
+}
