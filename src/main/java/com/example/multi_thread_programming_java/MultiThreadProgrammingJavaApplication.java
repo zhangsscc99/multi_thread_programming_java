503ea9121af139995cd334e225983d7e368cb14f
Yuting Wang <yw4554@nyu.edu>
2024-03-25 18:32:08 -0500

    multi-thread

---
+++ src/main/java/com/example/multi_thread_programming_java/MultiThreadProgrammingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.multi_thread_programming_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadProgrammingJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadProgrammingJavaApplication.class, args);
+    }
+
+}
