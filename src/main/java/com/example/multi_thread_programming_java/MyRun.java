109e42e405dc1e581a56d15419f2fb60b999fcbd
Yuting Wang <yw4554@nyu.edu>
2024-03-27 16:48:52 -0500

    multi-thread programming

--- src/main/java/com/example/multi_thread_programming_java/MyRun.java
+++ src/main/java/com/example/multi_thread_programming_java/MyRun.java
@@ -1,11 +1,15 @@
 package com.example.multi_thread_programming_java;
 
-public class MyRun extends Thread {
+public class MyRun implements Runnable {
     @Override
-    public void run() {
+    public void run() {//t1 //t2执行run方法里的代码
+
         for (int i = 0; i < 100; i++) {
-            Thread t1 = Thread.currentThread();
-            System.out.println("Hello world");
+            //获取到当前线程的对象
+
+            Thread t = Thread.currentThread();
+            //System.out.println(t.getName() + "Hello world");
+            System.out.println(Thread.currentThread().getName() + "HelloWorld");
         }
     }
 }
